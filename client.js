3c2de3247ef7aca1df156dc41c91e8d603795398
Natasha Chiu <chiuchiu07@gmail.com>
2023-07-06 02:52:20 +0800

    allow stdin to interact with server through connection object passed in

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const net = require("net");
 const connect = function() {
 
   const conn = net.createConnection({
-    host: "192.168.1.9",
+    host: "165.227.47.243",
     port: 50541
   });
 
--- input.js
+++ input.js
@@ -1,11 +1,29 @@
 // handle user input from stdin
 
+// store the active TCP connection object
+let connection;
+
 const handleUserInput = (data) => {
   if (data === "\u0003")
     process.exit();
+
+  if (data === "w")
+    connection.write("Move: up");
+
+  if (data === "a")
+    connection.write("Move: left");
+
+  if (data === "s")
+    connection.write("Move: down");
+
+  if (data === "d")
+    connection.write("Move: right");
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+
+  // place reference to conn object in global var
+  connection = conn;
 
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -13,8 +31,6 @@ const setupInput = function() {
   stdin.resume();
 
   stdin.on("data", handleUserInput);
-
-
   return stdin;
 };
 
