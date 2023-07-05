fc5fb08a3ecf179d80c45f6005c8c012c352e695
Natasha Chiu <chiuchiu07@gmail.com>
2023-07-06 01:54:22 +0800

    connect to server, receive data from server

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+const connect = function() {
+
+  const conn = net.createConnection({
+    host: "192.168.1.9",
+    port: 50541
+  });
+
+  conn.setEncoding("utf-8");
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+
+console.log("Connecting...");
+connect();
